adcc4d650fb11aac9238d9cad322a5331a5cff4f
andrewzhang451 <zhangandrew2020@gmail.com>
2024-09-25 21:55:03 -0500

    l5 - created 4 java class domains. l5 - specified the classes in persistence.xml file

---
+++ src/main/java/edu/iit/sat/itmd4515/azhang20/domain/ProductTracking.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.azhang20.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ *
+ * @author AndrewZ
+ */
+@Entity
+public class ProductTracking {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public ProductTracking(Long id) {
+        this.id = id;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+}
---
+++ src/main/java/edu/iit/sat/itmd4515/azhang20/domain/WarehouseManagement.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.azhang20.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ *
+ * @author AndrewZ
+ */
+@Entity
+public class WarehouseManagement {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public WarehouseManagement(Long id) {
+        this.id = id;
+    }
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    
+}
