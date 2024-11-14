2a90250b1917feba6264ed43e53178e03c415991
andrewzhang451 <zhangandrew2020@gmail.com>
2024-11-14 17:50:30 -0600

    l10 - got admin page to work

---
+++ src/main/java/edu/iit/sat/itmd4515/azhang20/web/AdminController.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.azhang20.web;
+
+import edu.iit.sat.itmd4515.azhang20.domain.Customer;
+import edu.iit.sat.itmd4515.azhang20.service.CustomerService;
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+import java.util.List;
+
+/**
+ *
+ * @author AndrewZ
+ */
+@Named
+@RequestScoped
+public class AdminController {
+    @EJB
+    private CustomerService customerService;
+
+    public List<Customer> getCustomers() {
+        return customerService.readAll();
+    }
+}
